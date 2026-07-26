0c4036f9333d7cb6dd1786d96c3f89e3456fb102
lukkyabdul <lukkyabdul2005@gmail.com>
2026-07-26 21:49:30 +0530

    LCS

---
+++ longestcommonsequence.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+                if prefix == "":
+                    return ""
+
+        return prefix
+
+
+# Driver Code
+obj = Solution()
+
+n = int(input("Enter number of strings: "))
+strs = []
+
+print("Enter the strings:")
+for i in range(n):
+    strs.append(input())
+
+print("Longest Common Prefix:", obj.longestCommonPrefix(strs))
